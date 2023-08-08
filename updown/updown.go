93ecb2bc394eb0767d129f37a9dc062fb919cf82
Daz Wilkin <daz.wilkin@gmail.com>
2023-08-07 18:57:12 -0700

    Removed `Event` to own file

--- updown/updown.go
+++ updown/updown.go
@@ -8,32 +8,6 @@ import (
 // https://updown.io/api
 // Each Webhook POST may contain multiple Events
 
-// Events is a type that represents multiple Event
-type Events []Event
-
-// Event is a type that represents an individual Webhook event
-type Event struct {
-	Event       string    `json:"event"`
-	Time        time.Time `json:"time"`
-	Description string    `json:"description"`
-	Check       Check     `json:"check,omitempty"`
-
-	// Only present with event 'check.performance_drop'
-	ApdexDropped string `json:"apdex_dropped,omitempty"`
-
-	// Union type
-	Downtime    Downtime             `json:"downtime,omitempty"`
-	SSL         SSL                  `json:"ssl,omitempty"`
-	LastMetrics map[time.Time]Metric `json:"last_metrics,omitempty"`
-}
-
-// Check is a type that represents an Updown check
-type Check struct {
-	Token      string `json:"token"`
-	URL        string `json:"url"`
-	LastStatus uint16 `json:"last_status"`
-}
-
 // Downtime is a type that represents the downtime of an Updown check
 type Downtime struct {
 	ID        string      `json:"id"`
@@ -41,7 +15,7 @@ type Downtime struct {
 	StartedAt time.Time   `json:"started_at"`
 	EndedAt   time.Time   `json:"ended_at"`
 	Duration  int         `json:"duration"`
-	Partial   interface{} `json:"partial"` // TODO:DazWilkin What type is this?
+	Partial   interface{} `json:"partial"` // TODO(dazwilkin) What type is this?
 }
 
 // SSL is a type that represents an Updown SSL record
