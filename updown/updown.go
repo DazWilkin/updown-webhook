f0ce437539d40d91b161416b8c825fc1341f0de5
Daz Wilkin <daz.wilkin@gmail.com>
2023-08-07 17:31:27 -0700

    Comments

--- updown/updown.go
+++ updown/updown.go
@@ -6,10 +6,12 @@ import (
 
 // Push API (webhooks)
 // https://updown.io/api
-
 // Each Webhook POST may contain multiple Events
+
+// Events is a type that represents multiple Event
 type Events []Event
 
+// Event is a type that represents an individual Webhook event
 type Event struct {
 	Event       string    `json:"event"`
 	Time        time.Time `json:"time"`
@@ -20,15 +22,19 @@ type Event struct {
 	ApdexDropped string `json:"apdex_dropped,omitempty"`
 
 	// Union type
-	Downtime    Downtime              `json:"downtime,omitempty"`
-	SSL         SSL                   `json:"ssl,omitempty"`
-	LastMetrics map[time.Time]Metrics `json:"last_metrics,omitempty"`
+	Downtime    Downtime             `json:"downtime,omitempty"`
+	SSL         SSL                  `json:"ssl,omitempty"`
+	LastMetrics map[time.Time]Metric `json:"last_metrics,omitempty"`
 }
+
+// Check is a type that represents an Updown check
 type Check struct {
 	Token      string `json:"token"`
 	URL        string `json:"url"`
 	LastStatus uint16 `json:"last_status"`
 }
+
+// Downtime is a type that represents the downtime of an Updown check
 type Downtime struct {
 	ID        string      `json:"id"`
 	Error     string      `json:"error"`
@@ -38,7 +44,7 @@ type Downtime struct {
 	Partial   interface{} `json:"partial"` // TODO:DazWilkin What type is this?
 }
 
-// SSL is a type that represents an Updown SSL certificate
+// SSL is a type that represents an Updown SSL record
 // Depending on the event type either Cert or NewCert|OldCert are present
 // Cert: check.ssl_invalid, check.ssl_valid, check.ssl_expiration
 // NewCert|OldCert: check.ssl_renewed
@@ -51,6 +57,8 @@ type SSL struct {
 	NewCert Cert `json:"new_cert,omitempty"`
 	OldCert Cert `json:"old_cert,omitempty"`
 }
+
+// Cert is a type that represents an Updown SSL certificate
 type Cert struct {
 	Subject   string    `json:"subject"`
 	Issuer    string    `json:"issuer"`
@@ -59,6 +67,7 @@ type Cert struct {
 	Algorithm string    `json:"algorithm"`
 }
 
-type Metrics struct {
+// Metric is a type that represents an Apdex metric
+type Metric struct {
 	Apdex float32 `json:"apdex"`
 }
